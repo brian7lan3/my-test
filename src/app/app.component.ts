b702fc3c682c78f44b459a21237c4b77ae182847
BoLan <brian7lan3@gmail.com>
2018-11-13 17:48:37 +0800

    class = {     css1: true,     css2: false   }

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,23 @@ import { removeDebugNodeFromIndex } from '@angular/core/src/debug/debug_node';
 })
 export class AppComponent {
 
-  class = true;
+  class = {
+    css1: true,
+    css2: false
+  }
 
 
 
   c1() {
-    this.class = true;
+    this.class.css1 = true;
+    this.class.css2 = false;
+  }
+  c2() {
+    this.class.css1 = false;
+    this.class.css2 = true;
   }
 
-  c2() {
-    this.class = false;
-  }
+
 
 
 }

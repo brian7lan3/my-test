4ac95769f517ba1f1fe33530402ef4b3267e69c4
BoLan <brian7lan3@gmail.com>
2018-11-13 18:54:02 +0800

    (click)="insert()">新增

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { removeDebugNodeFromIndex } from '@angular/core/src/debug/debug_node';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  name = "";
+  price = "";
+  rank: number;
 
   cpu = [
     { name: "i7-7700", price: "9800", rank: 1 },
     { name: "i5-7600", price: "7000", rank: 2 },
-    { name: "i3-7300", price: "5000", rank: 3 }
-  ];
+    { name: "i3-7300", price: "5000", rank: 3 },
+  ]
 }

3fd200de04770362b3eba444aeb42a11a3ee9961
BoLan <brian7lan3@gmail.com>
2018-11-13 16:25:01 +0800

    最簡化版本的 <button (click)="dd(divText, '你好')">你好</button>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  test = '測試';
-  name = 'Lue';
-  birthday = '12/2';
 
-  edit = false;
-  text = '編輯';
-
-  dd() {
-    this.edit = !this.edit;
-
-    if (this.text === '編輯') {
-      this.text = '確認';
-    } else {
-      this.text = '編輯';
-    }
-
+  dd(input: Document, abc: string) {
+    input.textContent = abc;
   }
-
 }

9d2082fbba5d8a56bcb1bb5ba899b189614560c6
BoLan <brian7lan3@gmail.com>
2018-11-13 14:53:00 +0800

    複習練習

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  test = "測試"; //初值
 
-
-  dd(divText: Document, text: string) {
-
-    divText.textContent = text;
+  call(money: any) {
+    alert("你給了" + money);
+    this.test = "你給了" + money;
   }
 }

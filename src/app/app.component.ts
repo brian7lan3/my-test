187bf3f5bbf8c4b363c068dcfbb4e9015f880409
BoLan <brian7lan3@gmail.com>
2018-11-13 18:23:30 +0800

    *ngIf="expression" //如果要用ngif的話，必須先用布林函數true false

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { removeDebugNodeFromIndex } from '@angular/core/src/debug/debug_node';
 })
 export class AppComponent {
 
+  //如果要用ngif的話，必須先用布林函數true false
+  edit = false;
+
   fun = [
     { name: "台灣", persent: "5%", rank: 0 },
     { name: "日本", persent: "87%", rank: 5 },
